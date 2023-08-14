5806550403b8fd16c9e4ad0d8bbf2138c2df97d6
SHUVO BISWAS <76837017+g0y3nd4@users.noreply.github.com>
2023-08-14 18:01:05 +0530

    discovery type update

--- client.py
+++ client.py
@@ -5,6 +5,7 @@ from scapy.all import sniff, Ether
 
 #/bin/python3 /home/user/test/client.py
 #---------------Analyisis packet & ex Ip and Port-------
+port=''
 stop_sniffing = False
 def packet_handler(packet):
     global ip_address
@@ -16,19 +17,37 @@ def packet_handler(packet):
         if hasattr(packet.payload, 'load'):
             payload_data = packet.payload.load.decode('utf-8', errors='ignore')
             print("Payload Data:", payload_data)
-            ip_address, port, null = payload_data.split(',')
 
             # regular expression
-            pattern = r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$'
-            if re.match(pattern, ip_address):
-                print("Match found:", ip_address, port)
+            pattern = r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}),(\d{1,5}),'
+            if re.match(pattern, payload_data):
+                ip_address, port, null = payload_data.split(',')
+                print(f"Match found & The IP: {ip_address} & Port:{port}")
                 stop_sniffing = True
             else:
                 print("No match.")
+
+#-----------------------------------------------------------------------------------------
+def discovery_type():
+    global ip_address
+    global port
+    print("You can select auto or manual discovery. For Auto select:1. Manual:2.")
+    choose = input("Enter your choose:")
+    if choose == "1":
+        print("Trying to auto Server discovery")
+        #----------------------Capture broadcast frame-------------------------------------------
+        sniff(iface='ens33', prn=packet_handler, stop_filter=lambda pkt: stop_sniffing, count=1000)
+    elif choose == "2":
+        ip_address = input("Enter the server ip address:")
+        port = input("Enter the port number:")
+        return port
+    else:
+        print("You choose a wrong options. Try again")
+
+
 #------------------------------------------------------------------------------------------
 def main():
-#----------------------Capture broadcast frame-------------------------------------------
-    sniff(iface='ens33', prn=packet_handler, stop_filter=lambda pkt: stop_sniffing, count=10)
+    discovery_type()
 #----------------Create clint---------------------------------------------
     port_int = int((port))
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
